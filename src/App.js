8e94c83aa14a2cff2db8db560ac421d30628c1bc
solomon-kimyubeen <kyb@solomontech.net>
2021-03-16 08:35:10 +0900

    hooks-tutorial

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Info from './Info';
+import Counter from './Counter';
+import Average from './Average';
+
+const App = () => {
+  return (
+    <div>
+      {/*info*/}
+      <Info />
+
+      {/*Counter*/}
+      <Counter/>
+
+      {/*Average*/}
+      <Average/>
+
+    </div>
+  )
+}
+
+export default App;
